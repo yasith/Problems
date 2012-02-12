671b737133e0b2de9cfe3797fbdd177f8c9c4ace
Yasith Vidanaarachchi <yasith.vidanaarachchi@utoronto.ca>
2012-02-11 23:10:42 -0500

    Started adding Problems from UVa Online Judge. (uva.onlinejudge.org)

---
+++ acm/3n+1.cpp
@@ -0,0 +1,59 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+
+#define p(x) cout << #x << ":" << x << endl;
+
+using namespace std;
+
+int dp[1000000];
+
+
+int rec(int x){
+  
+  if(x >= 1000000) return x % 2? rec(3*x + 1)+1: rec(x/2)+1;
+
+  if(dp[x] == 0){
+    dp[x] = x % 2 ? rec(3*x + 1)+1: rec(x/2)+1;
+  }
+  
+  return dp[x];
+}
+
+int main(){
+
+  dp[1] = 1;
+
+  int s, e;
+  while(cin >> s >> e){
+
+    int max_len = 0;
+
+    for (int i = min(s, e); i <= max(s, e); i++) {
+      max_len = max(max_len, rec(i));
+    }
+
+    printf("%d %d %d\n", s, e, max_len);
+  }
+
+  return 0;
+}
+
+
